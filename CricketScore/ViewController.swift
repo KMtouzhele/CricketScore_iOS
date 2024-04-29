082f0f4a2e94d785c49a5fabfc3c9a44420dec8a
KMtouzhele <kilmerau@hotmail.com>
2024-04-29 22:07:00 +1000

    Initial Commit

---
+++ CricketScore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CricketScore
+//
+//  Created by Kilmer Li on 2024/4/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
